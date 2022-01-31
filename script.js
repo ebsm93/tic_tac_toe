1d5e2b4168c63a0ab392fbdc9370781666cfb183
ebs <ebs.m93@gmail.com>
2022-01-31 10:45:04 -0800

    1%

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+(function () {
+
+	// submitNameBtn = document.querySelector("#submitNameBtn");
+	// name = document.querySelector('#nameInput').value;
+	// // x = 4;
+	// submitNameBtn.addEventListener('click', function() {submitName(name)});
+
+})()
+
+function submitName(name) {
+	console.log(name);
+	alert(name);
+}
+let x;
+let submitNameBtn = document.querySelector("#submitNameBtn");
+	// x = 4;
+submitNameBtn.addEventListener('click', function() {submitName(x)});
+x = document.querySelector('#nameInput').value;
+// x = 5;
+
+// name = document.querySelector('#nameInput').value;
+// name = 'yo';
+// submitName(name);
+
+
+// alert(name);
+
